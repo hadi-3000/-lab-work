138fb541724c63482c7b24ffa2412462fd16d415
hadi-3000 <adbulhadikawa@gmail.com>
2026-05-22 20:09:31 +0530

    addition prog

---
+++ q1.c
@@ -0,0 +1,10 @@
+// addition of two numbers.
+#include<stdio.h>
+int main(){
+    int x,y,sum;
+    printf("enter the two numbers :");
+    scanf("%d %d",&x,&y);
+    sum = x+y;
+    printf("the sum is %d",sum);
+    return 0;
+} 
